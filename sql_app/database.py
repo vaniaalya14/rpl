cf747d124c143ed76d07f220556c15d55ff2e9c0
vaniaalya14 <vaniaalya14@gmail.com>
2021-12-01 08:36:11 +0700

    update again

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import pymysql
+# import pymysql
 
 DATABASE_USER = "bowo"
 DATABASE_PASSWORD = "password"
 DATABASE_URI = "128.199.151.55"
 DATABASE_NAME = "rpl"
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_URI}:3306/{DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URL = f"mysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_URI}:3306/{DATABASE_NAME}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
