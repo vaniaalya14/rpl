40aa3d68fa88f0acaf7e11cbc0d5a2835a1bae86
vaniaalya14 <vaniaalya14@gmail.com>
2021-12-01 08:33:57 +0700

    import PyMySql

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import pymysql
 
 DATABASE_USER = "bowo"
 DATABASE_PASSWORD = "password"
